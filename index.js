629e6d7dc170a50727312147fe8c62558a9cdfca
Andrse <andrescandido2000@gmail.com>
2024-05-15 14:04:32 -0400

    Fixed Print button and added Download button for Resume

---
+++ index.js
@@ -0,0 +1,6 @@
+var doc = new jsPDF();
+
+function saveDiv(divId, title) {
+    doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+    doc.save('Andres Candido - Resume.pdf');
+}
